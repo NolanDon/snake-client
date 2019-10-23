420cc5bd1c634bcb1827b0a6f50d97734d70c75e
nolan <nolan796@live.ca>
2019-10-23 15:13:13 -0600

    Seperated Standard input files to seperate file

--- input.js
+++ input.js
@@ -1,2 +1,18 @@
-const handleUserInput = require('play');
-const setupInput = require('play');
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = function(){
+  if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!');
+    process.exit();
+  }
+}
+
+module.exports = setupInput;
