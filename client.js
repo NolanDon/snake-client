2c511fff66a4f58a0171a70425c7731c44da1006
nolan <nolan796@live.ca>
2019-10-23 16:28:53 -0600

    Allowed Modularity for IP and Host in designated folder

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
   const net = require('net');
+  const { IP, PORT } = require('./constants');
   const connect = function() {
   const conn = net.createConnection({ 
-    host: '192.168.15.225',
-    port: '50541'
+  host: IP,
+  port: PORT
   });
   conn.on('connect', (data) => {
     conn.write("Move: up");
